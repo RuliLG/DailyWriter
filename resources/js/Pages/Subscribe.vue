3156afe0960107f7c7441c562c0a7663bbbaa4a7
Raúl López <hi@raullg.com>
2022-09-21 23:12:50 +0100

    add price to subscription

--- resources/js/Pages/Subscribe.vue
+++ resources/js/Pages/Subscribe.vue
@@ -27,7 +27,7 @@
 
             <div class="flex justify-center mt-12">
                 <a :href="route('subscribe')" class="inline-flex text-center justify-center items-center px-4 py-2 bg-primary-700 border border-transparent rounded font-semibold text-sm text-white hover:bg-primary-600 active:bg-primary-800 focus:outline-none focus:border-primary-900 focus:shadow-outline-gray transition ease-in-out duration-150">
-                    Suscríbete ahora
+                    Suscríbete ahora (5€)
                 </a>
             </div>
         </div>
