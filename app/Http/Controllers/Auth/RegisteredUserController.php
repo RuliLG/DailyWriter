013f11b1caeb9d8a45f2a22dd54661d4c33e5083
Raúl López <hi@raullg.com>
2022-09-28 14:30:34 +0100

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'on_trial_until' => now()->addWeek(),
+            'trial_ends_at' => now()->addWeek(),
         ]);
 
         event(new Registered($user));
