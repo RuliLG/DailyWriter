132680c34ec2c163b9aeeb08cad9299f1bc92bfc
Raúl López <hi@raullg.com>
2022-10-04 09:14:21 +0100

    add analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,15 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Exo:wght@400;500;600;700&display=swap" rel="stylesheet">
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C5CGVJVTRF"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C5CGVJVTRF');
+        </script>
+
         <!-- Scripts -->
         @routes
         @vite('resources/js/app.js')
