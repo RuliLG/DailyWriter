14edd16c5ce5cb3f3b6358d3d04514d58ba92a83
Raúl López <hi@raullg.com>
2022-10-04 09:55:12 +0100

    responsive

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -3,12 +3,12 @@
         class="min-h-screen relative grid md:grid-cols-10"
         :class="[backgroundColor].join(' ')"
     >
-        <div class="col-span-6 px-32 relative flex items-center justify-center">
+        <div class="px-4 py-8 md:px-8 lg:px-16 xl:px-32 relative flex items-center justify-center md:col-span-5 lg:col-span-6">
             <div class="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed" :style="{ backgroundImage: `url('${backgroundImage}')`}"></div>
             <div class="bg-gradient-to-t from-gradient to-transparent absolute inset-0"></div>
             <slot name="banner"></slot>
         </div>
-        <div class="col-span-4 p-16 flex flex-col items-center justify-center">
+        <div class="p-4 order-first md:p-8 lg:p-16 flex flex-col items-center justify-center md:col-span-5 lg:col-span-4 md:order-none">
             <div class="w-full">
                 <Link href="/" class="block mx-auto">
                     <BreezeApplicationLogo class="mx-auto w-auto h-20 block" :class="[logoColor].join(' ')" />
