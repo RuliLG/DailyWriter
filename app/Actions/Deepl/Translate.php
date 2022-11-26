cbd110a1682b08bffe12a5d6d386a432406dffec
Raúl López <hi@raullg.com>
2022-11-26 17:40:48 +0000

    update deepl integration

--- app/Actions/Deepl/Translate.php
+++ app/Actions/Deepl/Translate.php
@@ -21,12 +21,13 @@ class Translate
         }
 
         $query = [
-            'text' => $text,
-            'target_lang' => 'en',
+            'text' => [$text],
+            'target_lang' => 'EN',
         ];
 
         $response = Http::withToken(config('services.deepl.token'), 'DeepL-Auth-Key')
-            ->post('https://api.deepl.com/v2/translate?' . http_build_query($query), [])
+            ->asForm()
+            ->post('https://api.deepl.com/v2/translate', $query)
             ->throw();
 
         $translation = new Translation;
